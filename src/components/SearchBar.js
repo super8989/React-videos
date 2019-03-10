2bba5d19fb60be1a4503919f0b30f96c87cff11f
Sam <super8989@gmail.com>
2019-03-10 16:22:32 -0400

    handling form submittal

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,20 +3,26 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: '' }; //short for searchterm
 
-  onInputChange = () => {
-
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
   }; //method on the class
+
+  onFormSubmit = event => {
+    event.preventDefault(); // no refresh after pressing enter on searchbar
+
+    //TODO: make sure we call callback from parent component
+  };
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form"> {/*onSubmit is a special name*/}
           <div className="field">
             <label>Video Search</label>
             <input
               type="text"
               value={this.state.term}
-              onChange={this.onInputChange} //onChange is a special name / onInputChange is a callback function on the SearchBar class
+              onChange={this.onInputChange} //onChange is a special name / onInputChange is a callback function on the SearchBar class / instead of this.onInputChange for this.setState, could've been e=> this.setState({ term: e.target.value })
             />
           </div>
         </form>
