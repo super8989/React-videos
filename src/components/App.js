4d169ccb5bd3fac89db6e6eb18a1859bb3ab011a
Sam <super8989@gmail.com>
2019-03-10 22:13:26 -0400

    fixing warnings and styling app

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import SearchBar from "./SearchBar";
-import youtube from '../apis/youtube';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async term => {
-    const response = await youtube.get('/search', {
+    const response = await youtube.get("/search", {
       params: {
         q: term
       }
@@ -18,15 +18,27 @@ class App extends React.Component {
   };
 
   onVideoSelect = video => {
-    this.setState({ selectedVideo: video })
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} /> {/*prop name could be anything*/}
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+        <SearchBar onFormSubmit={this.onTermSubmit} />{" "}
+        {/*prop name could be anything*/}
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
