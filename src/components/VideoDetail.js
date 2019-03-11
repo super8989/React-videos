cdfabb36b4d62cc19095e5ed48d4c3f429538775
Sam <super8989@gmail.com>
2019-03-10 21:57:31 -0400

    styling the video detail

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 const VideoDetail = ({ video }) => {
-  if(!video) {
+  if (!video) {
     return <div>Loading...</div>;
   }
 
   //props.video --> video
-  return <div>{video.snippet.title}</div>;
+  return (
+    <div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
